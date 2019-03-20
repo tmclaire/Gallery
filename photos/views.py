bc12f63527e12fb98230e483dd7a970f34c54711
NDOLIC <tblaguese1@gmail.com>
2019-03-20 13:05:08 +0200

    settings.py file we add the app configuration

--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import datetime as dt
 from __future__ import unicode_literals
 
 from django.shortcuts import render
@@ -9,3 +10,10 @@ from django.http  import HttpResponse
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to Gallery')
+
+
+def photos_of_day(request):
+    
+    date = dt.date.today()
+    photos = Image.objects.all()
+    return render(request, 'pictures/home.html', {"date": date,"photos":photos})
