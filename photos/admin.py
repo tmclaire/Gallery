fcdf5773ff5ecf4d2d1fbf69792446e3ca8c9b0d
NDOLIC <tblaguese1@gmail.com>
2019-03-21 11:27:39 +0200

    adding gallery admin

--- photos/admin.py
+++ photos/admin.py
@@ -4,6 +4,9 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import Location,Category,Image
 
+class GalleryAdmin(admin.ModelAdmin):
+    filter_horizontal =('Image',)
+
 admin.site.register(Location)
 admin.site.register(Category)
 admin.site.register(Image)
--- photos/urls.py
+++ photos/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    # url('^$',views.welcome,name = 'welcome'),
      url('^today/$',views.photos_of_day,name='photosToday')
 ]
