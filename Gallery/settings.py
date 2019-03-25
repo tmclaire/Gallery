a1b931d49e85bd5a593c014eb305d84de7b9ae21
NDOLIC <tblaguese1@gmail.com>
2019-03-25 11:42:07 +0200

    changes

--- Gallery/settings.py
+++ Gallery/settings.py
@@ -133,6 +133,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
