1b212f6de74cd09cac5895b83279b562c973f4b7
NDOLIC <tblaguese1@gmail.com>
2019-03-25 11:46:33 +0200

    settings changes

--- Gallery/settings.py
+++ Gallery/settings.py
@@ -126,12 +126,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
